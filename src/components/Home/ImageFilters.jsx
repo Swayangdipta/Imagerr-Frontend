e53bb2349f7b3180de927a4857f42907b0e0c59f
Swayangdipta Das <swayangdiptacc@gmail.com>
2023-01-22 18:21:42 +0530

    Removed unneccessary imports

--- src/components/Home/ImageFilters.jsx
+++ src/components/Home/ImageFilters.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const ImageFilters = () => {
   return (
-    <div>ImageFilters</div>
+    <div className='w-[30vw] h-max py-[20px] fixed top-[60px] right-0 rounded bg-emerald-300'>
+
+    </div>
   )
 }
 
