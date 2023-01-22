bb60d0cb7347cf33b3efda24c5b89d343c323f81
Swayangdipta Das <swayangdiptacc@gmail.com>
2023-01-22 12:01:49 +0530

    Added Header (In Progress)

--- src/components/base/Header.jsx
+++ src/components/base/Header.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { isAuthenticated } from '../../utils/LS_Helper'
+import Menu from './Menu'
+import {BiMenu} from 'react-icons/bi'
 
 const Header = ({currentLocation}) => {
+    const [isMenuOpen,setIsMenuOpen] = useState(false)
+    const {user} = isAuthenticated()
   return (
-    <div>Header</div>
+    <div className='w-[100vw] h-[60px] flex items-center justify-between bg-emerald-400 shadow-md fixed top-0'>
+        <section className='ml-[30px] py-0 my-0'>
+            <h1 className='text-[34px] py-0 mt-[-10px] font-[500]'>Imagerr</h1>
+        </section>
+        {
+            !user ? (
+                <section className='mr-[30px] text-[40px] text-zinc-900 cursor-pointer' onClick={e=>setIsMenuOpen(!isMenuOpen)}>
+                    <BiMenu />
+                </section>
+            ) : (
+                <section className='mr-[30px] flex gap-[20px]'>
+                    <button className='px-[20px] py-[5px] border-2 text-[17px] font-[400] border-emerald-900 rounded hover:bg-emerald-900 hover:text-white duration-300'>Sign up</button>
+                    <button className='px-[20px] py-[5px] text-white text-[18px] border-2 border-emerald-900 rounded bg-emerald-900 shadow-lg hover:shadow-none'>Sign in</button>
+                </section>
+            )
+        }
+        {
+            isMenuOpen && (<Menu currentLocation={currentLocation} />)
+        }
+    </div>
   )
 }
 
---
+++ src/components/base/Menu.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Menu = ({currentLocation}) => {
+  return (
+    <div className='fixed top-[65px] right-[30px] w-[300px] h-[200px] bg-emerald-400 rounded'>
+        Menu
+    </div>
+  )
+}
+
+export default Menu
