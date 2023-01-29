a22300a5831502d8560667efb26f1247713763a0
Swayangdipta Das <swayangdiptacc@gmail.com>
2023-01-29 17:38:56 +0530

    Fixed overflow issue

--- src/components/Image/ImageContainer.jsx
+++ src/components/Image/ImageContainer.jsx
@@ -5,7 +5,7 @@ import ImageCard from './ImageCard'
 
 const ImageContainer = () => {
   return (
-    <div className='w-[100%] h-[calc(100vh_-_90px)] overflow-y-scroll flex flex-wrap items-start gap-[10px] gap-x-auto gap-y-[10px] pb-[30px]'>
+    <div className='w-[100%] h-[calc(100vh_-_90px)] overflow-x-hidden overflow-y-scroll flex flex-wrap items-start gap-[10px] gap-x-auto gap-y-[10px] pb-[30px]'>
         <ImageCard image={{img:img1,author: "Swayangdipta Das",_id: "shuygsuhg672281bjnb"}} />
         <ImageCard image={{img:img2,author: "Swayangdipta Das",_id: "shuygsuhg672281bjnb"}} />
         <ImageCard image={{img:img1,author: "Swayangdipta Das",_id: "shuygsuhg672281bjnb"}} />
